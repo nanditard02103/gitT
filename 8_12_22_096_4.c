4b9bfc17a311a44ca4a5d976acfb25ff4332a5c1
nanditard02103 <120011041+nanditard02103@users.noreply.github.com>
2022-12-21 23:31:26 +0530

    Add files via upload

---
+++ 8_12_22_096_4.c
@@ -0,0 +1,73 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+#include<ctype.h>
+int stack[20];
+int top = -1;
+
+void push(int x)          //insert a element into stack
+{
+    stack[++top] = x;
+}
+
+int pop()              //removing an element from stack
+{
+    return stack[top--];
+}
+
+int main()   
+{
+    char exp[20];
+    char *e;
+    int n1,n2,n3,num;
+    printf("Enter the expression :: ");
+    scanf("%s",exp);
+    e = exp;
+    while(*e != '\0')
+    {
+        if(isdigit(*e))
+        {
+            num = *e - 48;
+            push(num);
+        }
+        else
+        {
+            n1 = pop();
+            n2 = pop();
+            switch(*e)            //for multiple options 
+            {
+            case '+':
+            {
+                n3 = n1 + n2;
+                break;
+            }
+            case '-':
+            {
+                n3 = n2 - n1;
+                break;
+            }
+            case '*':
+            {
+                n3 = n1 * n2;
+                break;
+            }
+            case '/':
+            {
+                n3 = n2 / n1;
+                break;
+            }
+            }
+            push(n3);
+        }
+        e++;
+    }
+    printf("\nThe result of expression %s  =  %d\n\n",exp,pop());
+    return 0;
+}
+
