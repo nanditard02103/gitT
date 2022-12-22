9acd8ceca53ed1fc6cd116a5d96ae86d340e843a
nanditard02103 <120011041+nanditard02103@users.noreply.github.com>
2022-12-22 16:27:30 +0530

    Add files via upload

---
+++ prg03a.c
@@ -0,0 +1,62 @@
+
+
+#include<stdio.h>
+#include<ctype.h>
+char stack[20];
+int top=-1;//initializing top value to -1//
+void push(char x)//user defined function//
+{
+  stack[++top]=x;
+}
+char pop()//user defined function//
+{
+ if(top==-1)
+  return -1;
+ else
+  return stack[top--]; 
+}
+int priority(char x)//user defined function//
+{
+  if(x=='(')
+   return 0;
+  if(x=='+' || x=='-')
+   return 1;
+  if(x=='*' || x=='/')
+   return 2; 
+}
+
+int main()
+{
+ char exp[30];
+ char *e,x;
+ printf("enter the expression\n");
+ scanf("%s",exp);
+ e=exp;
+ while(*e!='\0')
+ {
+   if(isalnum(*e))
+     printf("%c",*e);
+   else if(*e=='(')
+     push(*e);
+   else if(*e==')')
+   {
+      while((x==pop())!='(')
+       printf("%c",x);
+          
+   } 
+   else
+   {
+     while(priority(stack[top])>=priority(*e))
+      printf("%c",pop());
+      push(*e);
+            
+   }   
+   e++;
+ }
+ while(top!=-1)
+ {
+    printf("%c",pop());
+ }
+ return 0;
+}
+
