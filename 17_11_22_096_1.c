50e40001285bb1359b1fbaf9e6a661b214e24857
nanditard02103 <120011041+nanditard02103@users.noreply.github.com>
2022-12-22 16:08:15 +0530

    Add files via upload

--- 17_11_22_096_1.c
+++ 17_11_22_096_1.c
@@ -1,10 +1,4 @@
-/******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 //Program to implement a stack using array implementation//
 #include<stdio.h> //standard input output header file//
